43592eab76eaddbdb407059152849bab43386732
AKD47 <van83horn@gmail.com>
2018-01-25 15:33:16 +0300

    add preloader

--- public/js/script.js
+++ public/js/script.js
@@ -28,5 +28,27 @@ $(document).ready(function(){
         });
     }
     /*close*/
-
 });
+
+//preloader
+;(function ($) {
+
+    var preloader = {
+        open: function () {
+            $('body').addClass('hidden-overflow');
+            $('.js-open').fadeIn('fast');
+        },
+        close: function () {
+            $('body').removeClass('hidden-overflow');
+            $('.js-open').fadeOut('fast');
+        }
+    };
+
+    preloader.open();
+
+    $(window).on('load', function () {
+        setTimeout(function () {
+            preloader.close();
+        }, 400);
+    });
+})(jQuery);
